66e8ba0145b45a445e8e32b975f1ed74c69b8ab9
UsulPro <regx@usul.su>
2017-02-20 18:42:07 +0700

    API: initData 001

--- src/store/api.js
+++ src/store/api.js
@@ -2,12 +2,18 @@ import { loggerOn, loggerOff } from '../utils/logger'; // eslint-disable-line
 const logger = loggerOn; // note: debug
 
 export default {
-    incIndex(poddaStore, delta) {
-        poddaStore.update(state => ({ index: state.index + delta }));
+//    incIndex(poddaStore, delta) {
+//        poddaStore.update(state => ({ index: state.index + delta }));
+//    },
+//    setLabel(poddaStore, name, ind) {
+//        poddaStore.update(() => ({ index: ind, label: name }));
+//    },
+
+    dataInit(poddaStore, data) {
+        logger.log('dataInit:', data);
+        poddaStore.set('initData', data);
     },
-    setLabel(poddaStore, name, ind) {
-        poddaStore.update(() => ({ index: ind, label: name }));
-    },
+
     setData(poddaStore, data) {
         poddaStore.update(() => ({ data }));
         logger.log('Set Data:', poddaStore.getAll());
